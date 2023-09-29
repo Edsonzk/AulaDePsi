02b0408b9632f1f83d19c2416442fe9e5f85e779
Edson Filho <fernandoedson858@gmail.com>
2023-09-29 10:18:43 +0100

    atualizar o ola mundo

--- Semana01/OlaMundo/Program.cs
+++ Semana01/OlaMundo/Program.cs
@@ -2,10 +2,18 @@
 
 namespace OlaMundo
 {
-    class Program
+    /// <summary>
+    /// esse programa imprime a mensagem "hello world" ao user na consola
+    /// </summary>
+   class Program
     {
+        
+        /// <summary>
+        /// esse programa imprime a mensagem "hello world" ao user na consola
+        /// </summary>
         static void Main(string[] args)
         {
+            //este comando que permite imprimir a mensagem
             Console.WriteLine("Hello World!");
         }
     }
