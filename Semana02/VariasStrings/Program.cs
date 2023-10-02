f67821a63d3bfda7cd81ee18c7e72ea3c4eba308
Edson Filho <fernandoedson858@gmail.com>
2023-10-02 09:10:18 +0100

    adicionar strings2

--- Semana02/VariasStrings/Program.cs
+++ Semana02/VariasStrings/Program.cs
@@ -6,7 +6,17 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string astro = "Lua";
+            string n = @"this is \b";
+            string i = """Uma string entre aspas""";
+            string b = @"\u00A9 e \u2665";
+            string g = "\u00A9 e \u2665 sem UTF";
+
+            console.WriteLine(astro);
+            console.WriteLine(n);
+            console.WriteLine(i);
+            console.WriteLine(b);
+            console.WriteLine(g);
         }
     }
 }
