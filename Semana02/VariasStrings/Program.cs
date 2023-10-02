cdb99524a7007d9016ce779e97cee04e5b730e17
Edson Filho <fernandoedson858@gmail.com>
2023-10-02 10:22:14 +0100

    adicionar concatenações

--- Semana02/VariasStrings/Program.cs
+++ Semana02/VariasStrings/Program.cs
@@ -12,11 +12,29 @@ namespace VariasStrings
             string b = "\u00A9 e \u2665";
             string g = @"\u00A9 e \u2665 sem UTF";
 
+            string a = "a" + 2;
+            string c = "c" + x;
+
+            string si = $"{s} interpolado"
+            string ri = $@"{s} + {c} = {t} verbatim {x}";
+
+            string sf = String.Format("{0} e {1}", s, t);
+
+            Console.WriteLine("Valor de {0} mais {1} é {2}", x, y, x+y);
+
             Console.WriteLine(astro);
             Console.WriteLine(n);
             Console.WriteLine(i);
             Console.WriteLine(b);
             Console.WriteLine(g);
+            Console.WriteLine(a);
+            Console.WriteLine(c);
+            Console.WriteLine(si);
+            Console.WriteLine(ri);
+            Console.WriteLine(sf);
+
+
+
         }
     }
 }
