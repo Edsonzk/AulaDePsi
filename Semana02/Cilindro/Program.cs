f9a7f30fa6c25b4c8f127f528f82b45a91cb3ddd
Edson Filho <fernandoedson858@gmail.com>
2023-10-04 09:28:33 +0100

    adicionar cilindro

---
+++ Semana02/Cilindro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a;
+            double r; 
+            double pi = 3,14;
+            double V = (r * r) * a;
+            double Sa = 2 * 3,14 * r * (r * a);
+
+            Console.WriteLine("introduza a altura: ");
+            Console.WriteLine("introduza o raio: ");
+            Console.WriteLine(V);
+            Console.WriteLine(Sa);
+        }
+    }
+}
