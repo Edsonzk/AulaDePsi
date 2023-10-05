b71d1ae5cf6ed2a3a599150ac6bb91f74eb20ee2
Edson Filho <fernandoedson858@gmail.com>
2023-10-05 18:15:54 +0100

    adicionar cilindro

--- Semana02/Cilindro/Program.cs
+++ Semana02/Cilindro/Program.cs
@@ -6,15 +6,22 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
+            string sAltura, sRaio;
 
             double a;
-            double r; 
-            double pi = 3,14;
-            double V = (r * r) * a;
-            double Sa = 2 * 3,14 * r * (r * a);
+            double r, V, Sa; 
+            double pi = 3.1415926;
+            V = (r * r) * a;
+            Sa = 2 * 3,14 * r * (r * a);
 
             Console.WriteLine("introduza a altura: ");
-            Console.WriteLine("introduza o raio: ");
+            Console.WriteLine("introduza o raio do cilindro: ")
+            sAltura = Console.ReadLine();
+            sRaio = Console.ReadLine();
+
+            altura = double.Parse(sAltura);
+            raio = double.Parse(sRaio);
+
             Console.WriteLine(V);
             Console.WriteLine(Sa);
         }
