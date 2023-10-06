6929d4f892537d6a57bee0ef90cef50ccddecf73
Edson Filho <fernandoedson858@gmail.com>
2023-10-06 09:04:30 +0100

    adicionar pastas esquecidas

---
+++ Semana01/TiposVarios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 7;
+            uint ui = 7U;
+            long l = 142L;
+
+            float f = 15.27f;
+            double d =2.75;
+            decimal z = 53.213m;
+
+            bool b = true;
+            bool c = 4 > 5;
+
+            console.WriteLine(i);
+            console.WriteLine(ui);
+            console.WriteLine(l);
+
+            console.WriteLine();
+            
+            console.WriteLine(f);
+            console.WriteLine(d);
+            console.WriteLine(z);
+            console.WriteLine(b);
+            console.WriteLine(c);
+        }
+    }
+}
