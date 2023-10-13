1b38da6a6837dacc4436623264790c03965b23dd
Edson Filho <fernandoedson858@gmail.com>
2023-10-13 09:02:38 +0100

    adicionar omissor

---
+++ Semana03/Omissor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Omissor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string f;
+            string sc;
+            char c;
+
+            //solicita ao utilizador uma string para o programa
+            Console.WriteLine("Introduza uma frase: ");
+            f = Console.ReadLine();
+
+            Console.WriteLine("Introduza um caracter: ");
+            sc = Console.ReadLine();
+            c = char.Parse(sc);
+            foreach (char aC in f)
+            {
+                if(aC != c)
+                Console.Write(aC);
+            }
+
+        }
+    }
+}
