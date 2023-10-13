49c06e0531a6f33b2480aedbd972fe8421038a49
Edson Filho <fernandoedson858@gmail.com>
2023-10-13 08:33:12 +0100

    adicionar piramide

---
+++ Semana03/Piramide/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            string n;
+            // solicita o numero de linhas ao utilizador
+            Console.WriteLine("introduza o n de linhas: ");
+            Console.WriteLine();
+            n = Console.ReadLine();
+            b = Int32.Parse(n);
+
+
+
+            for (int i = 1; i <= b; i++)
+            {
+                for (int j = 1; j < b - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
