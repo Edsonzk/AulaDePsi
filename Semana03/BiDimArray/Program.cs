301db809df658c34222d41b8026026b061583b6d
Edson Filho <fernandoedson858@gmail.com>
2023-10-18 08:41:24 +0100

    adicionar biDim

---
+++ Semana03/BiDimArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BiDimArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            int dh, dv;
+            float[,] biDim;
+
+            Console.WriteLine("introduza a dimensão horizontal: ");
+            a = Console.ReadLine();
+            dh = int.Parse(a);
+            Console.WriteLine("introduza a dimensão vertical: ");
+            b = Console.ReadLine();
+            dv = int.Parse(b);
+
+            biDim = new float[dv, dh];
+
+            for (int i = 0; i < biDim.GetLength(0); i++)
+            {
+	             for (int c = 0; c < biDim.GetLength(1); c++)
+            {
+		         Console.Write(biDim[ i, c ]);
+	        }
+	             Console.WriteLine();
+            }
+
+            Console.WriteLine("");
+        }
+    }
+}
